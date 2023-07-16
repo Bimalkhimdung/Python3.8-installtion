1f3757b39cd72895d24802c063724ee617c373f3
Bimalkhimdung <bimalkhimdung@gmail.com>
2023-07-16 09:47:15 +0545

    File added

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#installing packages
+sudo apt update
+
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-devsudo apt install build-essential zlib1g-dev
+
+#dowload latest version of download
+
+curl -O https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tar.xz
+
+#download tar file
+
+tar -xf Python-3.8.2.tar.xz
+
+#navigate to the tar file
+
+cd Python-3.8.2
+
+./configure --enable-optimizatioins
+
+#Run make
+
+make -j 4
+
+#install python binaries
+
+sudo make altinstall && python3.8 --version
