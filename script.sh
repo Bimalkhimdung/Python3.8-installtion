4a5c99dc3e09fe427c61805202f7f1935e54f884
Bimal Rai <bimalkhimdung@gmail.com>
2023-07-17 15:07:00 +0545

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Author: Bimalkhimdung@gmail.com
 chmod +x script.sh
 
 #installing packages
@@ -8,7 +8,7 @@ sudo apt install curl -y
 sudo apt install make -y
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev && sudo apt install build-essential zlib1g-dev
 
-#dowload latest version of download
+#download the latest version of Python
 
 curl -O https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tar.xz
 
